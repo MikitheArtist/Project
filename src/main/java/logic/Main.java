debcc4ec09213602ab16512fd87c8d735acf98cf
Helen_Kerenykevych <helenchekanovska@gmail.com>
2022-04-21 22:38:24 +0300

    Initial commit

---
+++ src/main/java/logic/Main.java
@@ -0,0 +1,36 @@
+package logic;
+
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+
+import static logic.ExpressionAnalyze.expr;
+import static logic.ExpressionAnalyze.lexAnalyze;
+
+public class Main {
+    public static final String FILE_PATH_FOR_RESULT = "src/main/resources/textResult.txt";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String expressionText = scanner.nextLine();
+
+        List<Lexeme> lexemes = lexAnalyze(expressionText);
+        LexemeBuffer lexemeBuffer = new LexemeBuffer(lexemes);
+        String result = String.valueOf(expr(lexemeBuffer));
+
+        File file = new File(FILE_PATH_FOR_RESULT);
+
+        try {
+            PrintWriter printWriter =
+                    new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
+            printWriter.println(result);
+            printWriter.flush();
+            printWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
